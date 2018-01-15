394c3b9d82db21ce3207a3b9c9ea3e73187fb45e
Thomas <amphitheres@gmail.com>
2018-01-15 01:13:00 -0500

    It runs Setup with webpack now works. Can type > phaser-game@1.0.0 build /home/thomas/Documents/phaser-game > webpack
    Hash: 87803673112e216f8ae7
    Version: webpack 3.10.0
    Time: 1150ms
        Asset     Size  Chunks                    Chunk Names
    bundle.js  3.46 MB       0  [emitted]  [big]  main
       [0] (webpack)/buildin/global.js 509 bytes {0} [built]
       [1] ./src/app.js 793 bytes {0} [built]
        + 7 hidden modules to generate bundle.js

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 experimenting with phaser.io to make a game :video_game:.
 
 ## :fire: Setup :fire:
-first install `http-server` via
-`npm install -g http-server`.
+First run `npm install` to get all packages.
 
 Then in the root directory run `$: http-server`. Finally, open `localhost:8080` manually in your browser.
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-import "pixi";
-import "p2";
-import Phaser from 'phaser-ce';
+import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
+import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
+import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
+
+
 
 
 var PokeCard = {
